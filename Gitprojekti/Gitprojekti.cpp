4691a9405b595fc92b86a329a373ffc5358a48ca
Markus <makehuhtamaki@hotmail.com>
2020-01-16 12:13:03 +0200

    alku2

--- Gitprojekti/Gitprojekti.cpp
+++ Gitprojekti/Gitprojekti.cpp
@@ -3,6 +3,25 @@ using namespace std;
 
 int main()
 {
-    cout << "Nelilaskin!" << endl;
+    int a, b, toiminto;
+    cout << "Anna ensimmäinen luku: " << endl;
+    cin >> a;
+    cout << "Anna toinen luku: " << endl;
+    cin >> b;
+    cout << "Valitse toiminto: " << endl;
+    cin >> toiminto;
+
+    if (toiminto == 1) {
+        //plus
+    }
+    else if (toiminto == 2) {
+        //miinus
+    }
+    else if (toiminto == 3) {
+        //kerto
+    }
+    else {
+        //jako
+    }
 }
 
